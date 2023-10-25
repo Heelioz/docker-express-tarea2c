7371d89c2950899c6f3ac57ba1ed46d10cb0fbfb
Heelioz <115253511+Heelioz@users.noreply.github.com>
2023-10-25 02:52:55 +0000

    GET /directories, POST /directories, GET /directories/:id, PUT  /directories/:id, PATCH  /directories/:id, DELETE  /directories/:id

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+let directory = [];
+
+app.use(express.json())
 
 app.get('/', (_req, res) => {
   res.send('Running!')
@@ -8,6 +11,95 @@ app.get('/', (_req, res) => {
 
 app.get('/status/', (_req, res) => {
   res.json({result: 'pong'})
+})
+
+app.get('/directories/', (_req, res) => {
+  res.json({
+    count: directory.length,
+    next: directory.length > (10) ? "/directories/page/1": null,
+    previous: null,
+    results: directory.slice(0, 10)
+ })
+})
+
+app.get('/directories/page/:page', (req, res) => {
+  const currPage = req.params.page;
+  const prev = currPage > 1 ? `/directories/page/${currPage - 1}`: (currPage == 1 ? '/directories/': null );
+  const next = directory.length > ((currPage + 1) * 10) ? `/directories/page/${currPage + 1}`: null;
+  res.json({
+    count: directory.length,
+    next: next,
+    previous: prev,
+    results: directory.slice(currPage * 10, (currPage * 10) + 10)
+ })
+})
+
+app.post('/directories/', (req, res) => {
+  let newObject = req.body;
+  if (directory.length == 0) {
+    newObject.id = 0;
+  } else {
+    newObject.id = directory.at(-1).id + 1;
+  }
+  directory.push(newObject);
+  res.json({result: 'success'})
+})
+
+app.get('/directories/:id', (req, res) => {
+  for (let obj of directory) {
+    if (obj.id == req.params.id) {
+      res.json(obj);
+      return
+    }
+  }
+  res.status(404).json({result: 'not found'})
+})
+
+app.put('/directories/:id', (req, res) => {
+  const objId = req.params.id;
+  for (i = 0; i < directory.length; i++) {
+    const obj = directory[i];
+    if (obj.id == objId) {
+      let newObject = req.body;
+      newObject.id = objId;
+      directory[i] = newObject;
+      res.json({result: 'success'});
+      return
+    }
+  }
+  res.status(404).json({result: 'not found'})
+})
+
+app.patch('/directories/:id', (req, res) => {
+    const objId = req.params.id;
+    for (i = 0; i < directory.length; i++) {
+      if (directory[i].id == objId) {
+        let newModObject = req.body;
+        if (newModObject.name !== undefined && newModObject.name !== null) {
+          console.log(`obj ${objId} change name '${directory[i].name}' => ${newModObject.name}`);
+          directory[i].name = newModObject.name
+        }
+        if (newModObject.emails !== undefined && newModObject.emails !== null) {
+          console.log(`obj ${objId} change emails ['${directory[i].emails.toString()}'] => [${newModObject.emails.toString()}]`);
+          directory[i].emails = newModObject.emails
+        }
+        res.json({result: 'success'});
+        return
+      }
+    }
+    res.status(404).json({result: 'not found'})
+})
+
+app.delete('/directories/:id', (req, res) => {
+    const objId = req.params.id;
+    for (i = 0; i < directory.length; i++) {
+      if (directory[i].id == objId) {
+        directory.splice(i, 1);
+        res.json({result: 'success'});
+        return
+      }
+    }
+    res.status(404).json({result: 'not found'})
 })
 
 app.listen(port, () => {
